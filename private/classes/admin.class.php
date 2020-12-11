5543e4c6eea3f55520de83f0c69ea263772e1ace
justin <justinmhoch@gmail.com>
2020-12-11 09:42:08 -0500

    added forgotten semicolon

--- private/classes/admin.class.php
+++ private/classes/admin.class.php
@@ -21,7 +21,7 @@ class Admin extends DatabaseObject {
     $this->last_name = $args['last_name'] ?? '';
     $this->email = $args['email'] ?? '';
     $this->username = $args['username'] ?? '';
-    $this->user_level = $args['user_level' ?? 'm']
+    $this->user_level = $args['user_level' ?? 'm'];
     $this->password = $args['password'] ?? '';
     $this->confirm_password = $args['confirm_password'] ?? '';
   }
